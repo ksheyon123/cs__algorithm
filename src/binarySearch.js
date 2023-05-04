0452f9202e62362e3f3947d33b02ae6d9c5d7c01
Kangseohyeon <shkang@iconloop.com>
2023-05-04 20:03:38 +0900

    Solve binary search

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,8 +1,31 @@
 // What is the Binary Search?
 
-// > Binary search is an algorithm that finds a way to reach a target in a sorted list.;
+// > Binary search is an algorithm that finds a way to reach a target in a sorted list.
+// 
 
-const binarySearch = () => {
+/*
+Q. 1 ~ 10까지의 정렬된 수에서 target을 찾는 함수를 구하시오.
+**/
 
+
+const binarySearch = (nums, target) => {
+  let startIdx = 0;
+  let lastIdx = nums.length - 1;
+  let m = 0;
+  let iteration = 0;
+  while (startIdx <= lastIdx) {
+    m = Math.floor((startIdx + lastIdx) / 2);
+    if (nums[m] < target) {
+      startIdx = m + 1;
+    } else if (nums[m] > target) {
+      lastIdx = m - 1;
+    } else {
+      console.log("iteration : ", iteration, " idx : ", m);
+      return m;
+    }
+    iteration++;
+  }
 }
+
+binarySearch([1, 2, 3, 4, 5, 6], 1);
 
