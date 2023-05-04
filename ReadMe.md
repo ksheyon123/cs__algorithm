6758e6abf4c969548a1fc05d7753cf04b6f8e412
Kangseohyeon <shkang@iconloop.com>
2023-05-04 19:40:55 +0900

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+### Introduction
+
+> Weekly Algorithm 문제를 풀어나가려고 합니다.
+ 
+#### 2023/05/04
+
+- 이진 탐색 (Binary Search)
---
+++ src/binarySearch.js
@@ -0,0 +1,8 @@
+// What is the Binary Search?
+
+// > Binary search is an algorithm that finds a way to reach a target in a sorted list.;
+
+const binarySearch = () => {
+
+}
+
