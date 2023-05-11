f008b93b638b081b419162efddd7023b2a836095
Kangseohyeon <kshyeon123@gmail.com>
2023-05-11 15:00:53 +0900

    Fix insertion sort

--- src/sort.js
+++ src/sort.js
@@ -1,4 +1,5 @@
 const bubbleSort = (rnds)=> {
+  let iteration = 0;
 
   for (let i = 0; i <rnds.length; i++) {
     for (let j = i + 1; j < rnds.length; j++) {
@@ -6,12 +7,29 @@ const bubbleSort = (rnds)=> {
         const temp = rnds[j];
         rnds[j] = rnds[i];
         rnds[i] = temp;
+        iteration++;
       }
     }
   }
-  console.log(rnds);
+  console.log("bubble Sort ",rnds, iteration);
 }
 
-const rnds = [2, 1, 5, 4, 3, 6, 7, 10, 9, 8, 8];
+const insertionSort = (arr) => {
+  let iteration = 0;
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    const temp = arr[i];
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    iteration++;
+    arr[j + 1] = temp;
+  }
+  console.log("insertion Sort ",arr, iteration);
+}
+
+const rnds = [1, 5, 6, 2, 4, 3];
 
 bubbleSort(rnds);
+insertionSort(rnds);
