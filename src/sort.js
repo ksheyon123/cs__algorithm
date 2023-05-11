a63a282fe2dec8d5c66b73df072853be001f2298
Kangseohyeon <kshyeon123@gmail.com>
2023-05-11 14:18:51 +0900

    Success bubble sort

--- src/sort.js
+++ src/sort.js
@@ -1,11 +1,17 @@
-
-
 const bubbleSort = (rnds)=> {
 
-  
-
+  for (let i = 0; i <rnds.length; i++) {
+    for (let j = i + 1; j < rnds.length; j++) {
+      if(rnds[i] > rnds[j]) {
+        const temp = rnds[j];
+        rnds[j] = rnds[i];
+        rnds[i] = temp;
+      }
+    }
+  }
+  console.log(rnds);
 }
 
-const rnds = [2, 1, 5, 4, 3];
+const rnds = [2, 1, 5, 4, 3, 6, 7, 10, 9, 8, 8];
 
 bubbleSort(rnds);
